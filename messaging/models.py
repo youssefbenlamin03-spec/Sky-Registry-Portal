f08d69bc1425171fb5576a08ad6f2c64a82d0f8f
Iman Awan <maniawan@Imans-MacBook-Air.local>
2026-04-29 02:04:19 +0500

    Student 3 messaging feature

--- messaging/models.py
+++ messaging/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received')
+    subject = models.CharField(max_length=100)
+    content = models.TextField()
+    is_draft = models.BooleanField(default=False)
+    date_sent = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject 
